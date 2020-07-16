eb4dc75211cc3687aec217447959b9a87ea472f6
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-07-16 14:58:34 +0700

    add template blocked

---
+++ application/views/blocked.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>BSM - Rekonsel PKPA</title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?= base_url('assets/admin/plugins/fontawesome-free/css/all.min.css') ?>">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- icheck bootstrap -->
+	<link rel="stylesheet" href="<?= base_url('assets/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?= base_url('assets/admin/dist/css/adminlte.min.css') ?>">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+
+<body class="hold-transition login-page">
+	<div class="login-box">
+		<!-- Main Content -->
+		<div id="content">
+			<!-- Begin Page Content -->
+			<div class="container-fluid">
+				<!-- 404 Error Text -->
+				<div class="text-center">
+					<div class="error mx-auto">
+						<h1 style="font-size: 500%"><i class="fa fa-fw fa-exclamation-triangle"></i> 403</h1>
+					</div>
+					<h2 class="lead text-gray-800 mb-5" style="font-size: 300%">Access Forbidden</h2>
+				</div>
+			</div>
+			<!-- /.container-fluid -->
+		</div><br>
+		<!-- End of Main Content -->
+	</div>
+	<!-- /.login-box -->
+
+	<!-- jQuery -->
+	<script src="<?= base_url('assets/admin/plugins/jquery/jquery.min.js') ?>"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?= base_url('assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+	<!-- AdminLTE App -->
+	<script src="<?= base_url('assets/admin/dist/js/adminlte.min.js') ?>"></script>
+
+</body>
+
+</html>
